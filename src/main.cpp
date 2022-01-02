f82f85b827b8b8372f25a8879e78300b205e1e57
rabeesalman <38975018+rabeesalman@users.noreply.github.com>
2022-01-02 22:22:03 +0300

    ok

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,13 @@
 #include <PID_v1.h>
 #define PIN_OUTPUT 3
 double Setpoint, Input, Output;
-//double Kp = 10, Ki = 5, Kd = 6;
-double Kp = 6, Ki = 8, Kd = 0;
+// double Kp = 10, Ki = 5, Kd = 6;
+double Kp = 60, Ki = 5, Kd = 100;
 PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
-uint8_t detection = 0;
-int Value = 0;
-int last_CH1_state = 0;
+volatile bool detection = 0;
+volatile unsigned long Value = 0;
+volatile bool last_CH1_state = 0;
+//
 
 float stien_three(byte Thermistor_Pin, float Rdivid, float A, float B, float C)
 {
@@ -31,23 +32,42 @@ float stien_three(byte Thermistor_Pin, float Rdivid, float A, float B, float C)
   return T;
 }
 
+// // void rabee()
+// // {
+// //   if (!last_CH1_state)
+// //   {
+// //     detection = 1;
+// //   }
+// //   else if (last_CH1_state)
+// //   {
+// //     detection = 0;
+// //     last_CH1_state = 0;
+// //   }
+// // }
+
 void setup()
 {
-  pinMode(A4, INPUT);
+  pinMode(A5, INPUT);
   pinMode(PIN_OUTPUT, OUTPUT);
-  Serial.begin(9600);
-  Input = stien_three(A4, 10000.0, 7.8786994030E-04, 2.8985556847E-04, -1.3697290359E-07);
-  Setpoint = 36;
+  digitalWrite(PIN_OUTPUT, 0);
+  Serial.begin(1000000);
+  Input = stien_three(A5, 10000.0, 7.8786994030E-04, 2.8985556847E-04, -1.3697290359E-07);
+  Setpoint = 35.0;
   myPID.SetMode(AUTOMATIC);
   PCICR |= (1 << PCIE0);
   PCMSK0 |= (1 << PCINT0);
+  // attachInterrupt(digitalPinToInterrupt(3), rabee, RISING);
 }
+
 void loop()
 {
-  Input = stien_three(A4, 10000.0, 7.8786994030E-04, 2.8985556847E-04, -1.3697290359E-07);
+  Input = stien_three(A5, 10000.0, 7.8786994030E-04, 2.8985556847E-04, -1.3697290359E-07);
   myPID.Compute();
-  Value = map(Output, 0, 255, 5000, 10);
+  // Value = map(analogRead(A5), 250, 1000, 7200, 6);
   //  ....map(value, fromLow, fromHigh, toLow, toHigh)
+
+  Value = map(Output, 0, 255, 7200, 6);
+
   Serial.print(Input);
   Serial.print(" ");
   Serial.print(Output / 8);
@@ -57,28 +77,27 @@ void loop()
 
 ISR(PCINT0_vect)
 {
+
   if (PINB & B00000001)
   {
-    if (last_CH1_state == 0)
+    if (!last_CH1_state)
     {
       detection = 1;
     }
   }
-  else if (last_CH1_state == 1)
+  else if (last_CH1_state)
   {
     detection = 1;
     last_CH1_state = 0;
   }
-  while (detection)
+   if (detection)
   {
-    // delayMicroseconds(Value);
-    // digitalWrite(PIN_OUTPUT, HIGH);
-    // delayMicroseconds(100);
-    // digitalWrite(PIN_OUTPUT, LOW);
-    delayMicroseconds( Value);
+    delayMicroseconds(Value);
     digitalWrite(PIN_OUTPUT, HIGH);
-    delayMicroseconds(5000 -Value);
+    delayMicroseconds(100);
     digitalWrite(PIN_OUTPUT, LOW);
     detection = 0;
   }
 }
+
+
